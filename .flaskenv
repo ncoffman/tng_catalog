ca8d98f6b15eeca1cfd48ac6f0c8fefcb7ead89f
ncoffman <51147417+ncoffman@users.noreply.github.com>
2021-02-08 08:15:24 -0600

    Initial commit of hello world flask application
    -Added initial folder structure.
    -Added hello world flask application

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=tng_catalog.py
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app
+
+@app.route('/', methods=['GET'])
+@app.route('/index',methods=['GET'])
+
+def index():
+    return ("Hello world!")
